5e0e2f89f5e4ca8e6117f7513a8eed6800612f53
Your Name <your-email@example.com>
2026-05-08 10:21:28 +0530

    Fix build for render

--- backend/server.js
+++ backend/server.js
@@ -35,3 +35,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../dist/client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/client/index.html"));
+});
